81ae249aa9cb222f138c8e046fc6ad98969f889e
Brian Wallace <draygen80@gmail.com>
2025-08-12 14:09:53 -0400

    deploy config for Render

--- server/src/server.js
+++ server/src/server.js
@@ -1,20 +1,30 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
+const http = require('http');
+
 const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const server = http.createServer(app);
+const io = require('socket.io')(server);
 
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-app.get('/', (req, res) => {
-  res.send('Server is running!');
+
+// Serve the built client
+const clientDist = path.resolve(__dirname, '../../client/dist');
+app.use(express.static(clientDist));
+
+// SPA fallback (put API routes above this)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(clientDist, 'index.html'));
 });
 
 io.on('connection', (socket) => {
-  console.log('A user connected');
+  console.log('A user connected', socket.id);
+  socket.on('disconnect', () => console.log('User disconnected', socket.id));
 });
 
-http.listen(port, '0.0.0.0', () => {
+server.listen(port, '0.0.0.0', () => {
   console.log(`Server is listening on port ${port}`);
 });
